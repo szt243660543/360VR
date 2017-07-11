1503863abc774d44afebbf0821a9136edc1248b4
szt243660543 <243660543@qq.com>
2017-07-11 15:23:09 +0800

    Initial Commit

---
+++ VR_Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VR_Example
+//
+//  Created by szt on 2017/7/11.
+//  Copyright © 2017年 VR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
